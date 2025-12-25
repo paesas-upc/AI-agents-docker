53ecab3b26a59a37e9e073cf16ed5c32d27c7611
Pau Escobar Asensio <pau.escobar.asensio@estudiantat.upc.edu>
2025-12-25 11:38:33 +0100

    git

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,13 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
 
+MY_PROJECT = os.environ.get("MY_PROJECT") or "This is my project"
+API_KEY = os.environ.get("API_KEY")
+if not API_KEY:
+    raise NotImplementedError("API_KEY was not set")
+
 @app.get("/")
 def read_index():
-    return {"hello":"world again"}
+    return {"hello":"world", "project-name":MY_PROJECT, "API_KEY":API_KEY}
